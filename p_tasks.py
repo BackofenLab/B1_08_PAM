7167969ac4a59aba8aee00f262f0719c9cc95143
Alexander-Mitrofanov <mitrofan@informatik.uni-freiburg>
2021-12-10 00:46:06 +0100

    programming: all the steps done

--- p_tasks.py
+++ p_tasks.py
@@ -1,4 +1,6 @@
 from math import log10
+import numpy as np
+
 
 def helper_index(char):
     return "ACTG".index(char)
@@ -41,7 +43,8 @@ def scores_calculation_correct(sim_align):
     return scores_matrix
 
 
-def calculate_gamma(freq_mutations_matrix):
+def gamma_calculation_correct(sim_align):
+    freq_mutations_matrix = mutation_calculation_correct(sim_align)
     sum_non_diagonal = sum([column for row_index, row in enumerate(freq_mutations_matrix)
                             for column_index, column in enumerate(row)
                             if row_index != column_index])
@@ -50,7 +53,7 @@ def calculate_gamma(freq_mutations_matrix):
     return gamma
 
 
-def calculate_probabilities_correct(sim_align):
+def probabilities_calculation_correct(sim_align):
     freq_nuc = nucleotide_freq_calculation_correct(sim_align)
     freq_matrix = mutation_calculation_correct(sim_align)
 
@@ -61,10 +64,10 @@ def calculate_probabilities_correct(sim_align):
     return probability_matrix
 
 
-def calculate_norm_probabilities_correct(sim_align):
-    probability_matrix = calculate_probabilities_correct(sim_align)
+def norm_probabilities_calculation_correct(sim_align):
+    probability_matrix = probabilities_calculation_correct(sim_align)
     freq_mutations = mutation_calculation_correct(sim_align)
-    gamma = calculate_gamma(freq_mutations)
+    gamma = gamma_calculation_correct(freq_mutations)
 
     probability_matrix = [[probability_matrix[row_index][column_index] * gamma
                            for column_index, column in enumerate(row)]
@@ -80,6 +83,23 @@ def calculate_norm_probabilities_correct(sim_align):
     return probability_matrix
 
 
+def pam_calculation_correct(sim_align, power):
+    freq_nuc = nucleotide_freq_calculation_correct(sim_align)
+    probability_matrix = norm_probabilities_calculation_correct(sim_align)
+
+    prob = probability_matrix
+    for i in range(power-1):
+        prob = np.dot(probability_matrix, prob)
+
+    pam = [[x for x in row] for row in prob]
+    pam = [[pam[row_index][column_index] / freq_nuc[column_index]
+            for column_index, column in enumerate(row)]
+           for row_index, row in enumerate(probability_matrix)]
+    pam = [[round(10 * log10(x)) for x in row] for row in pam]
+
+    return pam
+
+
 def main():
     seq1 = "AAGTACTTTAGGTAACACGTTTAGTCAAAATTCCTAAGTTTACCGGGTTAATCA"
     seq2 = "AAATTCCTAAGTTTACCGGGTTAATCAAAGTACTTTAGGTAACACGTTTAGTCA"
@@ -89,12 +109,10 @@ def main():
     print(nucleotide_freq_calculation_correct(sim_align))
     print(mutation_calculation_correct(sim_align))
     print(scores_calculation_correct(sim_align))
-
-    m_matrix = mutation_calculation_correct(sim_align)
-    print(calculate_gamma(m_matrix))
-
-    print(calculate_probabilities_correct(sim_align))
-    print(calculate_norm_probabilities_correct(sim_align))
+    print(gamma_calculation_correct(sim_align))
+    print(probabilities_calculation_correct(sim_align))
+    print(norm_probabilities_calculation_correct(sim_align))
+    print(pam_calculation_correct(sim_align, 1))
 
 
 if __name__ == "__main__":
